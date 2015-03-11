ad0d41e417402c353d5f5a44581aa1d679dc1d4c
Dmitrii Cucleschin <dmitrii.cucleschin@gmail.com>
2015-03-11 19:08:14 +0100

    one line cookie fix

--- client/app/app.js
+++ client/app/app.js
@@ -7,9 +7,10 @@ angular.module('jRoomsApp', [
   'ui.router',
   'ui.bootstrap'
 ])
-  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+  .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider
       .otherwise('/');
 
     $locationProvider.html5Mode(true);
+    $httpProvider.defaults.withCredentials = true;
   });
