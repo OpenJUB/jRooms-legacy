fce59c13a6ff018c55989bc3b922d35ac3e58d61
Vlad Ungureanu <vvu@vdev.ro>
2015-04-15 02:10:17 +0200

    Added endpoint to handle college map.
    Signed-off-by: Vlad Ungureanu <vvu@vdev.ro>

--- server/api/room/index.js
+++ server/api/room/index.js
@@ -6,5 +6,6 @@ var controller = require('./room.controller');
 var router = express.Router();
 
 router.get('/get', controller.getRoom);
+router.get('/getCollegeMap', controller.getCollegeMap);
 
 module.exports = router;
--- server/api/room/room.controller.js
+++ server/api/room/room.controller.js
@@ -2,6 +2,7 @@
 
 var _ = require('lodash');
 var Room = require('./room.model');
+var map = require('../../config/rooms/rooms.js');
 
 exports.getRoom = function(req, res) {
   var roomName = req.query.room;
@@ -12,4 +13,29 @@ exports.getRoom = function(req, res) {
 
     return res.json(200, room);
   })
+}
+
+exports.getCollegeMap = function(req, res) {
+	var college = req.query.college;
+	var retMap = map[0];
+
+	if (college == "krupp")
+		return res.json(200, retMap);
+
+	if (college == "mercator") {
+		retMap = map[3];
+		return res.json(200, retMap);
+	}
+
+	if (college == "c3") {
+		retMap = map[2];
+		return res.json(200, retMap);
+	}
+
+	if (college == "nordmetall") {
+		retMap = map[1];
+		return res.json(200, retMap);
+	}
+
+	return res.json(500, "Bad");
 }
