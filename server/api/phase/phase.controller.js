9a182063928859f1965266601c834db17e8d702b
filipbitola <filipstankovski@yahoo.com>
2015-04-17 02:21:04 +0200

    Unallocated endpoint fix

--- server/api/phase/phase.controller.js
+++ server/api/phase/phase.controller.js
@@ -177,7 +177,7 @@ exports.allResults = function(req, res) {
 
 
 exports.unallocated = function(req, res) {
-  User.find({$where: "this.college_preference.length === 0" }).exec(function(err, u) {
+  User.find({$where: "this.nextCollege !== null" }).exec(function(err, u) {
     if(err) {
       return res.json(500, err);
     }
